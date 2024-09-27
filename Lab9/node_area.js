74614859ba8ae3674cc8420308818d04d89cb90d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-27 12:39:37 +0530

    node_area.js 2

--- Lab9/node_area.js
+++ Lab9/node_area.js
@@ -1,16 +1,26 @@
-const area = require("./area");
+var http = require("http");
+var area = require("./area");
 
 const radius = 5;
 const length = 10;
 const width = 6;
 const side = 4;
 
-const circleArea = area.circle(radius);
+const circleArea = area.circle(radius).toFixed(2);
 const rectangleArea = area.rectangle(length, width);
 const squareArea = area.square(side);
 
-console.log(`Area of Circle with radius ${radius}: ${circleArea.toFixed(2)}`);
-console.log(
-  `Area of Rectangle with length ${length} and width ${width}: ${rectangleArea}`
-);
-console.log(`Area of Square with side ${side}: ${squareArea}`);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Area Calculations</h1>");
+    res.write(`<p>Area of Circle with radius ${radius}: ${circleArea}</p>`);
+    res.write(
+      `<p>Area of Rectangle with length ${length} and width ${width}: ${rectangleArea}</p>`
+    );
+    res.write(`<p>Area of Square with side ${side}: ${squareArea}</p>`);
+    res.end();
+  })
+  .listen(8080);
+
+console.log("Server running at http://localhost:8080/");
