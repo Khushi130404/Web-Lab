5668f14a80f6bd38abcefa732cded82f55ca9552
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-27 12:33:38 +0530

    area.js

---
+++ Lab9/area.js
@@ -0,0 +1,13 @@
+module.exports = {
+  circle: function (radius) {
+    return Math.PI * radius * radius;
+  },
+
+  rectangle: function (length, width) {
+    return length * width;
+  },
+
+  square: function (side) {
+    return side * side;
+  },
+};
